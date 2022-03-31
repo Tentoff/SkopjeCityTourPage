5180b25edc20059968e3ef330e49c7ee825d8429
Tentoff <tentov@outlook.com>
2022-03-31 12:01:03 +0200

    Initial commit

---
+++ main.js
@@ -0,0 +1,23 @@
+
+        function validateForm() {
+        if (isEmpty(document.getElementById('data_3').value.trim())) {
+        alert('Name is required!');
+        return false;
+        }
+        if (!validateEmail(document.getElementById('data_5').value.trim())) {
+        alert('Email must be a valid email address!');
+        return false;
+        }
+        if (isEmpty(document.getElementById('data_6').value.trim())) {
+        alert('Date is required!');
+        return false;
+        }
+        if (isEmpty(document.getElementById('data_7').value.trim())) {
+        alert('Time is required!');
+        return false;
+        }
+        return true;
+        }
+        function isEmpty(str) { return (str.length === 0 || !str.trim()); }
+       
+      
